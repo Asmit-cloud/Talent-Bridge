d48b3e9480c6e0dc9c9fca1b995bbca000a3e6b9
Asmit-cloud <asmitdey10@gmail.com>
2025-07-02 18:20:15 +0530

    Update `build.sh`
    Removed the "...--fake" line from the `build.sh` file once the problem solved.

--- build.sh
+++ build.sh
@@ -4,8 +4,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py migrate SkillSwap_Network 0011_remove_message_room_remove_message_sender_and_more --fake
-
 python manage.py migrate
 
 python manage.py collectstatic --no-input
