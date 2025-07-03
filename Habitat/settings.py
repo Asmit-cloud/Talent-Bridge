284fdb716fdf9557e2543626f09a39321b1ac4bd
Asmit-cloud <asmitdey10@gmail.com>
2025-07-03 07:52:30 +0530

    Use WhiteNoise
    Installed `WhiteNoise`, updated the `requirements.txt` and set the correct settings in `settings.py`.

--- Habitat/settings.py
+++ Habitat/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -147,6 +148,9 @@ STATICFILES_DIRS = [
 
 # This is the directory where 'collectstatic' will gather all static files
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# Add this for WhiteNoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
